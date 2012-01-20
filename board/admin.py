497f3d2410f323f126bb8b36f95afc32af2cdd67
Luis Peralta <lperalta@darkstar.(none)>
2012-01-20 12:25:21 +0100

    added category support

--- board/admin.py
+++ board/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from board.models import Service, Status, Event
+from board.models import Category, Service, Status, Event
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
 
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
--- board/models.py
+++ board/models.py
@@ -1,6 +1,23 @@
 from datetime import datetime, date, timedelta
 from django.db import models
 
+
+class Category(models.Model):
+    """
+    A category grouping services
+    """
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    description = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'categories'
+    
+    def __unicode__(self):
+        return self.name
+
+
 class Service(models.Model):
     """
     A service to track.
@@ -8,6 +25,7 @@ class Service(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField()
     description = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, related_name='services', null=True)
 
     class Meta:
         ordering = ('name',)
