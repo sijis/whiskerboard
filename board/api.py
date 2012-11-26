bc3bfccaf49d12f8081e768716393bd31ef5a98f
Sijis Aviles <sijis.aviles+github@gmail.com>
2012-11-25 21:59:07 -0600

    added authentication and authorization

--- board/api.py
+++ board/api.py
@@ -2,12 +2,29 @@ from tastypie import fields
 from tastypie.resources import ModelResource
 from board.models import Service, Category, Status, Event
 from tastypie.constants import ALL, ALL_WITH_RELATIONS
+from tastypie.authentication import BasicAuthentication
+from tastypie.authorization import DjangoAuthorization
+
+# Authentication class noted from http://stackoverflow.com/a/12273403
+class SimpleAuthentication(BasicAuthentication):
+    '''
+    Authenticates everyone if the request is GET otherwise performs
+    BasicAuthentication.
+    '''
+
+    def is_authenticated(self, request, **kwargs):
+        if request.method == 'GET':
+            return True
+        return super(SimpleAuthentication, self).is_authenticated(request, **kwargs)
+
 
 class CategoryResource(ModelResource):
     class Meta:
         queryset = Category.objects.all()
         resource_name = 'categories'
-        excludes = ['slug', 'id']
+        excludes = ['id']
+        authentication = SimpleAuthentication()
+        authorization = DjangoAuthorization()
         filtering = {
             "name": ALL,
         }
@@ -19,7 +36,9 @@ class ServiceResource(ModelResource):
     class Meta:
         queryset = Service.objects.all()
         resource_name = 'services'
-        excludes = ['slug', 'id']
+        excludes = ['id']
+        authentication = SimpleAuthentication()
+        authorization = DjangoAuthorization()
         filtering = {
             "name": ALL,
         }
@@ -29,11 +48,14 @@ class ServiceResource(ModelResource):
         bundle.data['current-event'] = bundle.obj.current_event()
         return bundle.data
 
+
 class StatusResource(ModelResource):
     class Meta:
         queryset = Status.objects.all()
         resource_name = 'statuses'
-        excludes = ['slug', 'id']
+        excludes = ['id']
+        authentication = SimpleAuthentication()
+        authorization = DjangoAuthorization()
         filtering = {
             "name": ALL,
         }
@@ -41,11 +63,13 @@ class StatusResource(ModelResource):
 
 class EventsResource(ModelResource):
 
-    #service = fields.ForeignKey(ServiceResource, 'service', full=True)
+    service = fields.ForeignKey(ServiceResource, 'service')
     status = fields.ForeignKey(StatusResource, 'status', full=True)
 
     class Meta:
         queryset = Event.objects.all()
         resource_name = 'events'
-        excludes = ['slug', 'id']
+        excludes = ['id']
+        authentication = SimpleAuthentication()
+        authorization = DjangoAuthorization()
 
