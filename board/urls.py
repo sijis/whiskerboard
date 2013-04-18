ae672c7923d2b5b1522579ce272b53e37b3440a8
Sijis Aviles <sijis.aviles+github@gmail.com>
2013-04-18 22:02:37 +0000

    added ability to filter services by year, month and day

--- board/urls.py
+++ board/urls.py
@@ -12,6 +12,9 @@ v1_api.register(EventsResource())
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})$', ServiceView.as_view(), name='service'),
+    url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})$', ServiceView.as_view(), name='service'),
+    url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})$', ServiceView.as_view(), name='service'),
     url(r'^services/(?P<slug>[-\w]+)$', ServiceView.as_view(), name='service'),
     url(r'^feed$', EventFeed(), name='feed'),
     url(r'^api/', include(v1_api.urls)),
