aa9b9c79751c17b382143deb08018f088aaccba5
Sijis Aviles <sijis.aviles+github@gmail.com>
2017-02-04 02:30:47 -0600

    fix: Remove usage of patterns

--- board/urls.py
+++ board/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from board.feeds import EventFeed
 from board.views import IndexView, ServiceView
 from board.api import ServiceResource, CategoryResource, StatusResource, EventsResource
@@ -13,7 +13,7 @@ v1_api.register(CategoryResource())
 v1_api.register(StatusResource())
 v1_api.register(EventsResource())
 
-urlpatterns = patterns('',
+urlpatterns = [
                        url(r'^$', IndexView.as_view(), name='index'),
                        url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})$', ServiceView.as_view(), name='service'),
                        url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})$', ServiceView.as_view(), name='service'),
@@ -22,4 +22,4 @@ urlpatterns = patterns('',
                        url(r'^feed$', EventFeed(), name='feed'),
                        url(r'^api/', include(v1_api.urls)),
                        url(r'^admin/', include(admin.site.urls)),
-                       )
+                       ]
