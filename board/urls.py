d7554cbaa64f5df689a4c983e32b1cb52f27827e
Sijis Aviles <sijis.aviles+github@gmail.com>
2017-02-11 17:45:12 -0600

    chore: reformat file

--- board/urls.py
+++ board/urls.py
@@ -14,12 +14,12 @@ v1_api.register(StatusResource())
 v1_api.register(EventsResource())
 
 urlpatterns = [
-                       url(r'^$', IndexView.as_view(), name='index'),
-                       url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})$', ServiceView.as_view(), name='service'),
-                       url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})$', ServiceView.as_view(), name='service'),
-                       url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})$', ServiceView.as_view(), name='service'),
-                       url(r'^services/(?P<slug>[-\w]+)$', ServiceView.as_view(), name='service'),
-                       url(r'^feed$', EventFeed(), name='feed'),
-                       url(r'^api/', include(v1_api.urls)),
-                       url(r'^admin/', include(admin.site.urls)),
-                       ]
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})$', ServiceView.as_view(), name='service'),
+    url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})$', ServiceView.as_view(), name='service'),
+    url(r'^services/(?P<slug>[-\w]+)/(?P<year>\d{4})$', ServiceView.as_view(), name='service'),
+    url(r'^services/(?P<slug>[-\w]+)$', ServiceView.as_view(), name='service'),
+    url(r'^feed$', EventFeed(), name='feed'),
+    url(r'^api/', include(v1_api.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+]
